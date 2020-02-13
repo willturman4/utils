cb51736d336aa5ec196b55abfb7516b1b36e4e4f
davidho503 <59979210+davidho503@users.noreply.github.com>
2020-02-13 15:46:32 -0600

    Add files via upload

--- utilsTester.c
+++ utilsTester.c
@@ -1,28 +1,29 @@
 
 #include<stdio.h>
-#include "sum.h"
+#include "utils.h"
+#include<math.h> 
 
 int main(void) {
 	double originLatitude=15;
 	double originLongitude=15;
 	double destinationLatitude=15;
 	double destinationLongitude=16;
-	
+
 	double expected_getAirDistance=107.4059;
 	double resultA=getAirDistance(originLatitude, originLongitude, destinationLatitude, destinationLongitude);
-	if (fabs(expected_getAirDistance-result)<=0.0001) {
-		printf("Passed")
+	if (fabs(expected_getAirDistance-resultA)<=0.0001) {
+		printf("Passed");
 	}
 	else{
 		printf("Failed");
 	}
-	double t=2
-	double percentC=.25
-	
-	double expected_lorentzTimeDilation=
+	double t=2;
+	double percentC=.25;
+
+	double expected_lorentzTimeDilation=2.3094;
 	double resultB=lorentzTimeDilation(t, percentC);
 	if (fabs(expected_lorentzTimeDilation-resultB)<=.0001) {
-		printf("Passed")
+		printf("Passed");
 	}
 	else{
 		printf("Failed");
